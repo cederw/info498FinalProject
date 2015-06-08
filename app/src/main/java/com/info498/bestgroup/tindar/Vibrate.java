c311e09ef63908352cbd32c97ec4a5004da877ab
Alex <alex_ngo@live.com>
2015-06-07 22:26:01 -0700

    got sliders

--- app/src/main/java/com/info498/bestgroup/tindar/Vibrate.java
+++ app/src/main/java/com/info498/bestgroup/tindar/Vibrate.java
@@ -2,27 +2,47 @@ package com.info498.bestgroup.tindar;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.gc.materialdesign.views.ButtonRectangle;
+import com.gc.materialdesign.views.Slider;
 
 
 public class Vibrate extends Activity {
 
-    private int sliderCount;
+    private int sliderCount = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vibrate);
-        final RelativeLayout sliderGroup = (RelativeLayout) findViewById(R.id.sliderGroup);
+        Slider slider = (Slider) findViewById(R.id.vibrateSlider);
 
-        final com.gc.materialdesign.views.ButtonRectangle vibrate = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.vibrateButton);
+        slider.setValue(2);
+
+        Typeface robotoFont = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Bold.ttf");
+
+        TextView text = (TextView) findViewById(R.id.vibrateText);
+        text.setTypeface(robotoFont);
+
+        slider.setOnValueChangedListener(new Slider.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(int i) {
+                sliderCount = i;
+            }
+        });
+
+        final ButtonRectangle vibrateButton = (ButtonRectangle) findViewById(R.id.vibrateButton);
 
 
-        vibrate.setOnClickListener(new View.OnClickListener() {
+        vibrateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Tindar.ConnectedThread connectedThread = ((Tindar) getApplication()).connectedThread;
@@ -33,6 +53,6 @@ public class Vibrate extends Activity {
                 }
             }
         });
-        vibrate.setRippleSpeed(80f);
+        vibrateButton.setRippleSpeed(80f);
     }
 }
